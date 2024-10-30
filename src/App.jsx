bb5735d3f0c390893ca98d7475f69bdbf1a37c99
satyam soni <satyamsoni853@gmail.com>
2024-10-30 12:10:44 +0530

    updated routes and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect,
+  Navigate,
+} from "react-router-dom";
 
 import Location from "./Location/Location";
 import Navbar from "./Navigation/Navbar";
@@ -8,22 +14,23 @@ import Product from "./OurProduct/Product";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 import Landing from "./Landing/Landing";
+import Layout from "./Layout";
 
 function App() {
   return (
     <div>
-       
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />} />
-        <Route path="/about" element={<About />} /> 
-        <Route path="/product" element={<Product />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/contact" element={<Contact />} />
-       </Routes> 
-    </BrowserRouter>
-    
-     
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace={true} />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="home" element={<Landing />} />
+            <Route path="about" element={<About />} />
+            <Route path="product" element={<Product />} />
+            <Route path="location" element={<Location />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import './Navbar.css';
-import Navimages from './navimage.png'
+import "./Navbar.css";
+import Navimages from "./navimage.png";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
@@ -12,34 +12,43 @@ function Navbar() {
     console.log(showsidebarr);
 
     setShowsidebarr(!showsidebarr);
-  }
+  };
   return (
-   <div>
-     <div className="Navbarr">
-      <ul>      
-        <li>EverGreenCare</li>
-        <li className="nav-image" ><img src={Navimages} alt="" /></li>
-        <li><Link to="/About" >About</Link></li>
-        <li><Link to="/product" >Product</Link></li>
-        <li><Link to="/location" >Location</Link></li>
-        <li><Link to="/contact" >ContactUs</Link></li>
-        <li className="cimenubarr" onClick={handleHamBurger} >
-        {showsidebarr ? <CiMenuBurger /> : <IoCloseSharp />}
-        </li>
-        
-      </ul>
-    </div>
-    {showsidebarr ? null : (
+    <div>
+      <div className="Navbarr">
+        <ul>
+          <li>
+            <Link to="/home">EverGreenCare</Link>
+          </li>
+          <li className="nav-image">
+            <img src={Navimages} alt="" />
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/product">Product</Link>
+          </li>
+          <li>
+            <Link to="/location">Location</Link>
+          </li>
+          <li>
+            <Link to="/contact">ContactUs</Link>
+          </li>
+          <li className="cimenubarr" onClick={handleHamBurger}>
+            {showsidebarr ? <CiMenuBurger /> : <IoCloseSharp />}
+          </li>
+        </ul>
+      </div>
+      {showsidebarr ? null : (
         <ul className="close-barr">
           <li>About</li>
-        <li>Our product</li>
-        <li>Loacation</li>
-        <li>Contact us</li>
+          <li>Our product</li>
+          <li>Loacation</li>
+          <li>Contact us</li>
         </ul>
       )}
-    
-      
-   </div>
+    </div>
   );
 }
 
